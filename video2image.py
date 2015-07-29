eab5b9d8fa278a88698be82d1cc903f96791bcb2
Zieghailo <isakov.m@gmail.com>
2015-07-30 01:49:46 +0200

    Skipping frames working

--- video2image.py
+++ video2image.py
@@ -4,27 +4,31 @@ __author__ = 'zieghailo'
 import cv2
 
 
-def video2frames(path):
+def video2frames(path, skip=1):
     video_object = cv2.VideoCapture(path)
 
-    index = 1
+    index = 0
     while True:
-        success,frame = video_object.read()
+        success, frame = video_object.read()
         if success:
-            # assumes that the extension is three letters long
-            cv2.imwrite(path[:-4] + "_" + str(index) + ".jpg", frame)
-            index += 1
+            if index % skip == 0:
+                cv2.imwrite(path[:-4] + "_" + str(index) + ".jpg", frame)  # assumes that the extension is three letters long
         else:
             break
+
+        index += 1
 
 
 def main():
     import argparse
     parser = argparse.ArgumentParser("Enter the filename of a video")
     parser.add_argument('filename')
+    parser.add_argument('--skip')
     args = parser.parse_args()
 
-    video2frames(args.filename)
+    if args.skip is None:
+        args.skip = 1
+    video2frames(args.filename, int(args.skip))
 
 if __name__ == "__main__":
     main()
